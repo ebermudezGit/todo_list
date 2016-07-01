cc3fe13859e4867174765069f1943dbf3b6a3e5b
Eli Bermudez <bermudez.86@live.com.mx>
2016-07-01 02:10:26 -0500

    Rules Business

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701064104) do
+ActiveRecord::Schema.define(version: 20160701070503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20160701064104) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -42,5 +45,6 @@ ActiveRecord::Schema.define(version: 20160701064104) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lists", "users"
   add_foreign_key "tasks", "lists"
 end
